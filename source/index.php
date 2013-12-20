59875ce2af3ee5eaf24ce4c9eec995e599b9ee8f
Eduardo Sebastian <esebastian@altiora.es>
2013-12-20 12:59:50 +0100

    Initial commit, original site

---
+++ source/index.php
@@ -0,0 +1,41 @@
+<?header("location:home.htm")?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Bicitaller Russafa</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+.Estilo1 {
+	font-family: verdana;
+	font-size: 9px;
+	color: #FFC600;
+}
+-->
+</style>
+</head>
+
+<body>
+<table cellspacing=0 cellpadding=0 align="center" height="100%">
+<tr><td width="100%" height="100%" align="center" valign="middle">
+<table width="980" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+	<img src="imagenes/bicitaller_index.gif" border="0" usemap="#Map" href="home_nu.htm">
+	</td>
+  </tr>
+</table>
+</td>
+</tr>
+</table>
+
+<map name="Map">
+  <area shape="rect" coords="447,299,538,333" href="home.htm">
+</map>
+<div style="position:absolute; width:100%; text-align:center; top:345px; left:2px; color:orange; font:10px verdana">
+<br>Visitas desde el 01/02/2008<br><br>
+<?include_once("contador.php")?>
+</div>
+</body>
+</html>
